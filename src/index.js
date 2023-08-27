fba5098fcbca7454652b5d9a6eff457c2c2212a0
akasharyanofficial7 <akasharyangupta7@gmail.com>
2023-08-27 22:14:59 +0530

    setup sequelize db

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-require("dotenv").config();
-
+const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+
 const setupandStartServer = async () => {
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   app.listen(PORT, () =>
     console.log(
       `Server is running on this is good  hi kya haal chal lalu port ${PORT}`
